695aecad17b81fa6a963199f72e3a4c9f897095a
tt-cell <2577037830@qq.com>
2019-09-10 09:31:52 +0800

    Initial commit

---
+++ src/main/java/com/evaluate/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.evaluate.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@MapperScan("com.evaluate.demo.dao")
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}

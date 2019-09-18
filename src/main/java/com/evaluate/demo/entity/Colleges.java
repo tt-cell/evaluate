a18bb9f61f133478d282196b3b0359278a0d11a9
linlijinzhi <25800810006@qq.com>
2019-09-18 09:21:00 +0800

    9.18版本

---
+++ src/main/java/com/evaluate/demo/entity/Colleges.java
@@ -0,0 +1,22 @@
+package com.evaluate.demo.entity;
+
+public class Colleges {
+    private int colleges_id;
+    private String colleges_name;
+
+    public int getColleges_id() {
+        return colleges_id;
+    }
+
+    public void setColleges_id(int colleges_id) {
+        this.colleges_id = colleges_id;
+    }
+
+    public String getColleges_name() {
+        return colleges_name;
+    }
+
+    public void setColleges_name(String colleges_name) {
+        this.colleges_name = colleges_name;
+    }
+}
--- src/main/java/com/evaluate/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/evaluate/demo/service/Impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.evaluate.demo.service.Impl;
 
 
 import com.evaluate.demo.dao.UserDao;
+import com.evaluate.demo.entity.Colleges;
 import com.evaluate.demo.entity.User;
 
 import com.evaluate.demo.service.UserService;
@@ -13,7 +14,69 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserDao userDao;
 
 
+    @Override
+    public int insertOneUser(String uname, String password) {
+        return userDao.insertOneUser(uname,password);
+    }
+
+    @Override
+    public List<User> selectCheckUsername(String uname) {
+        return null;
+    }
+
+    @Override
+    public List<User> selectCheckUser(String uname, String password) {
+        return null;
+    }
+
+    @Override
+    public int updateOneUser(String uname, int uid) {
+        return userDao.updateOneUser(uname,uid);
+    }
+
+    @Override
+    public List<User> selectAllUser() {
+        return userDao.selectAllUser();
+    }
+
+    @Override
+    public int deleteOneUser(int uid) {
+        return userDao.deleteOneUser(uid);
+    }
+
+    @Override
+    public List<User> selectOneUser(int uid) {
+        return userDao.selectOneUser(uid);
+    }
+
+    @Override
+    public List<User> findAllPage(int before, int after) {
+        return userDao.findAllPage(before,after);
+    }
+
+    @Override
+    public int count() {
+        return userDao.count();
+    }
+
+    @Override
+    public int insertOneUser(String uname,String password,int sex,int colleges_id){
+        return userDao.insertOneUser(uname,password,sex,colleges_id);
+    }
+
+    @Override
+    public List<Colleges> selectAllColleges() {
+        return userDao.selectAllColleges();
+    }
+
+    @Override
+    public int selectOneColleges_id(String Colleges_name) {
+        return userDao.selectOneColleges_id(Colleges_name);
+    }
 }
